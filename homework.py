423988e31c2a33d0c5b3f3a3582a8d25de16388d
Nikita <savvin.nikit@yandex.ru>
2021-02-14 23:41:06 +0300

    1

--- homework.py
+++ homework.py
@@ -1,6 +1,7 @@
+import logging
 import os
 import time
-import logging
+
 import requests
 import telegram
 from dotenv import load_dotenv
@@ -11,33 +12,43 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-URL_YP = https://praktikum.yandex.ru/api/user_api/homework_statuses/
+URL_YP = f'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
-class HomeworkStatus(Exception):
-    pass
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    try:
-        if homework.get('homework_name') != 'rejected':
-            verdict = 'К сожалению в работе нашлись ошибки.'
+    status = homework.get('status')
+    if not homework_name or not status:
+        message = 'Не удалось получить статус'
+        raise Exception(message)
+    pages = {
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'reviewing': 'Работа взята в ревью',
+        'approved': 'Ревьюеру всё понравилось, можно приступать к следующему уроку.',
+    }
+    if status in pages:
+        verdict = pages[status]
+        if status == 'approved':
+            answer = f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
         else:
-            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-    except HomeworkStatus:
-        return f'Домашнюю работу "{homework_name}" ещё не взяли в работу или не проверили или возникла ошибка при отправке'
+            answer = pages[status]
+    else:
+        answer = f'Статус работы "{homework_name}" неизвестен'
+    #return answer - такой вариант не пропускает pytest, но мне кажется более универсальным
+    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     headers = {f'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    params = {'frome_date': current_timestamp}
+    params = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(URL_YP, params=params, headers=headers)
+        homework_statuses = requests.get(
+            URL_YP, params=params, headers=headers
+        )
         return homework_statuses.json()
     except requests.RequestException as error:
         logging.exception("Exception occurred")
-        print(f'Ошибка: {error}') 
-
+        raise error
 
 
 def send_message(message, bot_client):
@@ -47,16 +58,20 @@ def send_message(message, bot_client):
 
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
     logging.debug('Запуск NextBot')
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]), bot_client)
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]), bot_client
+                )
+            current_timestamp = new_homework.get(
+                'current_date', current_timestamp
+            )
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
